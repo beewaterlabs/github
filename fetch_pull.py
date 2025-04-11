9437922da0cd4c77a463a039b3d77a8d10177d96
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 23:40:15 -0700

    Adding: etch_pull.py, pull_remote_push_local_email_slicer_script.py, auto_push.py

--- fetch_pull.py
+++
@@ -1,6 +0,0 @@
-# This will fetch and pull from GitHub repo and then push any local changes:
-
-import subprocess
-
-# Execute all git commands in sequence
-subprocess.run("git fetch && git pull && git status", shell=True)
--- pull_remote_push_local_email_slicer_script.py
+++
@@ -1,8 +0,0 @@
-# This will fetch and pull from GitHub repo and then push any local changes:
-
-import subprocess
-
-# Execute all git commands in sequence
-subprocess.run("git pull && git add email_slicer.py && git commit -a -m \"Adding email_slicer.py\" && git push && git status", shell=True)
-
-
