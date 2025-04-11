e1b7ef7f72ce33daecc46cde7a86b9e440da9d5e
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 23:10:01 -0700

    Update pull_remote_push_local_script.py

--- pull_remote_push_local_script.py
+++ pull_remote_push_local_script.py
@@ -1,6 +1,8 @@
 # This will fetch and pull from GitHub repo and then push any local changes:
 
-git pull; git add email_slicer.py; git commit -a -m "Adding email_slicer.py"; git push; git status
+import subprocess
 
-git log
+# Execute all git commands in sequence
+subprocess.run("git pull && git add email_slicer.py && git commit -a -m \"Adding email_slicer.py\" && git push && git status", shell=True)
+
 
