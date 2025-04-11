5c1f9d5385e7d617eef1591b4e86c7848ccb2f2c
beewaterlabs <160707500+beewaterlabs@users.noreply.github.com>
2025-04-10 23:01:46 -0700

    Update pull_remote_push_local_script.py
    Added: git log

--- pull_remote_push_local_script.py
+++ pull_remote_push_local_script.py
@@ -1,3 +1,6 @@
 # This will fetch and pull from GitHub repo and then push any local changes:
 
  git fetch; git pull; git add email_slicer.py; git commit -a -m "Adding email_slicer.py"; git push; git status
+
+git log
+
